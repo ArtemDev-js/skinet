9a7b9e61ab87997bff5564211aae4bd7b1076acb
Artem Voitovych <avoitovych@dxc.com>
2020-12-25 13:16:46 +0100

    End of section 5

---
+++ API/Errors/ApiException.cs
@@ -0,0 +1,12 @@
+﻿namespace API.Errors
+{
+    public class ApiException : ApiResponse
+    {
+        public ApiException(int statusCode, string message = null, string detailes = null) : base(statusCode, message)
+        {
+            Details = detailes;
+        }
+
+        public string Details { get; set; }
+    }
+}
---
+++ API/Errors/ApiResponse.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+
+namespace API.Errors
+{
+    public class ApiResponse
+    {
+        public ApiResponse(int statusCode, string message = null)
+        {
+            StatusCode = statusCode;
+            Message = message ?? GetDefaultMessageForStatusCode(statusCode);  // ?? means "if this is null execute what to the right after question marks"
+        }
+
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+
+        private string GetDefaultMessageForStatusCode(int statusCode)
+        {
+            return statusCode switch
+            {
+                400 => "A bad request",
+                401 => "Not Authorized",
+                404 => "Resource not found",
+                500 => "Server error",
+                _ => null  //default case
+            };
+        }
+    }
+}
