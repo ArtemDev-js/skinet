f293cebac6c258b09a47a9b400953fc32a7eae67
Artem Voitovych <avoitovych@dxc.com>
2021-01-18 19:59:07 +0100

    End of section 13

--- client/src/app/core/Interceptors/loading.interceptors.ts
+++ client/src/app/core/Interceptors/loading.interceptors.ts
@@ -6,9 +6,8 @@ import { BusyService } from "../services/busy.service";
 
 @Injectable()
 export class LoadingInterceptor implements HttpInterceptor {
-    constructor(private busyService: BusyService){
-        
-    }
+    constructor(private busyService: BusyService){ }
+    
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         this.busyService.busy();
         return next.handle(req).pipe(
